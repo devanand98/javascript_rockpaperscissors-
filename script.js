bc70016cec0f14138c7cb72d7adc7b2cbcf23393
Dev <devanandv12@gmail.com>
2022-04-07 17:10:19 +0530

    Added endgame function

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ function computerPlay(){
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function startGame(){
-    let playerWin = 0 ;
-    let computerWin=0;
-    let gameWinner ='';
+let playerWin = 0 ;
+let computerWin=0;
+let gameWinner ="";
 
+function startGame(){
+    
+   
     //add event listeners for all three buttons 
 
         const buttons = document.querySelectorAll('button');
@@ -17,91 +19,132 @@ function startGame(){
             button.addEventListener('click',() =>{
                 playerSelection=button.id;
                 const computerSelection = computerPlay();
-                console.log(playRound(computerSelection,playerSelection));
-                battleWinText= (playRound(computerSelection,playerSelection));
-                
-                playerWinText='Player Win Total: '+ playerWin;
-                computerWinText= 'Computer Win Total: ' + computerWin;
+                battleWinText.textContent= (playRound(computerSelection,playerSelection));
+                playerWinText.textContent='Player Win Total: '+ playerWin;
+                computerWinText.textContent= 'Computer Win Total: ' + computerWin;
+                endGame();
 
-        })
-    }
+                });
+         });
 
     function playRound(computerSelection,playerSelection){
 
         //PC SELECTS ROCK
     
         if (computerSelection == "rock" && playerSelection == "rock") {
-            return "Tie";   
+            alert("Draw. Both of you played Rock");   
         }
         else if (computerSelection == "rock" && playerSelection == "paper"){
-            return "Player";
+            playerWin++;
+            alert("You win! Paper beats Rock!");
         }
         else if (computerSelection == "rock" && playerSelection == "scissors"){
-            return "Computer";
+            computerWin++;
+            alert("You Lose! Rock beats Scissors");
         }
         //PC SELECTS PAPER
     
         else if (computerSelection == "paper" && playerSelection == "rock"){
-            return "Computer";
+            computerWin++;
+            alert("You Lose! Paper beats Rock");
         }
         else if (computerSelection == "paper" && playerSelection == "paper"){
-            return "Tie";
+            alert("Draw! Both Picked Paper");
         }
         else if (computerSelection == "paper" && playerSelection == "scissors"){
-            return "Player";
+            playerWin++;
+            alert("You Win! Scissors beats Paper");
         }
     
         //PC SELECTS SCISSORS
     
         else if (computerSelection == "scissors" && playerSelection == "scissors"){
-            return "Tie";
+            alert("Draw! Both picked Scissors");
         }
         else if (computerSelection == "scissors" && playerSelection == "paper"){
-            return "Computer";
+            computerWin++;
+            alert("You Lose! Scissors beats Paper");
         }
         else if (computerSelection == "scissors" && playerSelection == "rock"){
-            return "Player";
+            playerWin++;
+            alert("You Win! Rock beats Scissors");
         }
     };
 
 
+    //DOM Manipulation
+    const container = document.querySelector('#container');
+    const resultsDiv = document.createElement('div');
+    resultsDiv.style.marginTop='20px';
+    container.appendChild(resultsDiv);
+    
+    //player win tracking dom
+    let playerWinText = document.createElement('p');
+    playerWinText.style.color='blue';
+    resultsDiv.appendChild(playerWinText);
+
+    //computer win tracking dom
+    let computerWinText = document.createElement('p');
+    computerWinText.style.color='blue';
+    resultsDiv.appendChild(computerWinText);
+
+    //battle win dom
+    const battleWinText =document.createElement('p');
+    battleWinText.style.color='black';
+    resultsDiv.appendChild(battleWinText);
+
+    //game win dom
+    const gameWinText = document.createElement('p');
+    gameWinText.style.color="orange";
+    resultsDiv.appendChild(gameWinText);
+
+    function endGame(){
+        if (playerWin == 5){
+            gameWinner = "You win!";
+            gameWinText.textContent=gameWinner;
+    
+            //disable buttons
+            document.getElementById('rock').disabled = true;
+            document.getElementById('paper').disabled = true;
+            document.getElementById('scissors').disabled = true;
+    
+            //Create replay button
+            const playAgainButton=document.createElement('button');
+            playAgainButton.textContent="Play Again";
+            resultsDiv.appendChild(playAgainButton);
+    
+            //Make button reload page
+            playAgainButton.addEventListener('click',()=>{
+                location.reload();
+            });
+    
+        }else if(computerWin == 5){
+            gameWinner="You Lose!";
+            gameWinText.textContent = "YOU LOSE";
+    
+            //disable buttons
+            document.getElementById('rock').disabled = true;
+            document.getElementById('paper').disabled = true;
+            document.getElementById('scissors').disabled = true;
+    
+              //Create replay button
+              const playAgainButton=document.createElement('button');
+              playAgainButton.textContent="Play Again";
+              resultsDiv.appendChild(playAgainButton);
+      
+              //Make button reload page
+              playAgainButton.addEventListener('click',()=>{
+                  location.reload();
+              });
+        };
+    };
+    
+    
+
+};
 
 
-
-
-
-
-
-
-
-//DOM Manipulation
-const container = document.querySelector('#container');
-const resultsDiv = document.createElement('div');
-resultsDiv.style.marginTop='20px';
-container.appendChild(resultsDiv);
-
-//player win tracking dom
-const playerWinText = document.createElement('p');
-playerWinText.style.color='blue';
-//playerWinText.textContent = "Player wins: " + playerWin;
-resultsDiv.appendChild(playerWinText);
-
-//computer win tracking dom
-const computerWinText = document.createElement('p');
-computerWinText.style.color='blue';
-//computerWinText.textContent = "Computer wins: " + computerWin;
-resultsDiv.appendChild(computerWinText);
-
-//battle win dom
-const battleWinText =document.createElement('p');
-battleWinText.style.color='black';
-resultsDiv.appendChild(battleWinText);
-
-//game win dom
-const gameWinText = document.createElement('p');
-gameWinText.style.color="orange";
-//gameWinText.textContent=gameWinner;
-resultsDiv.appendChild(gameWinText);
+startGame();
 
 
 
